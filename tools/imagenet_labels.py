b8b3eabe52305e90d5610e0b80f0928dfdb88274
nithin6114 <kemasaramn@iitbhilai.ac.in>
2025-04-24 00:26:23 +0530

    removedg inception.py

--- tools/imagenet_labels.py
+++ tools/imagenet_labels.py
@@ -8,7 +8,7 @@ _lut = [
     'stingray',
     'cock',
     'hen',
-    'ostrih, Struthio camelus',
+    'ostrich, Struthio camelus',
     'brambling, Fringilla montifringilla',
     'goldfinch, Carduelis carduelis',
     'house finch, linnet, Carpodacus mexicanus',
